707888eea6c7c5e79431579be05e0720962be9c1
shivemind <shivehive@gmail.com>
2022-07-16 13:06:43 -0700

    go

--- app.js
+++ app.js
@@ -24,3 +24,57 @@ app.use('/article', newsRouter)
 
 // Listen on port 5000
 app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
+
+
+//Here is your listener.
+app.use(express.json())
+//middleware to read req.body.<params>
+//CREATE USER
+app.post("/createUser", async (req,res) => {
+const user = req.body.name;
+
+function doPost(e) {
+    var isProduction = false;
+    
+    var strSimulator  = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
+    var strLive = "https://ipnpb.paypal.com/cgi-bin/webscr";
+    var paypalURL = strSimulator;
+    
+    if (isProduction)  paypalURL = strLive;
+    
+    var payload = "cmd=_notify-validate&" + e.postData.contents;
+  
+    var options =
+      {
+        "method" : "post",
+        "payload" : payload,
+      };
+      
+    var resp = UrlFetchApp.fetch(paypalURL, options); //Handshake with PayPal - send acknowledgement and get VERIFIED or INVALID response
+    
+    if (resp == 'VERIFIED') {
+      if (e.parameter.payment_status  == 'Completed') {
+        if (e.parameter.receiver_email == 'receiver@email.com') {
+          //Implement paid amount validation. If accepting payments in multiple currencies, use e.parameter.exchange_rate to convert to reference currency (USD) if paid in any other currency
+          if (amountValid) {
+          
+            //All validated - can process the payment here
+  
+            if (!(processSuccess)) {
+              //Process of payment failed - raise notification to check it out
+            }
+          } else {
+            //Payment does not equal expected purchase value
+          }
+        } else {
+         //Request did not originate from my PayPal account 
+        }
+      } else {
+       //Payment status not Completed 
+      }
+    } else
+    {
+     //PayPal response INVALID 
+    }
+  }
+})
