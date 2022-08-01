2b848428b7f2a002e0a6c499dae0bffb093e4afc
shivemind <shivehive@gmail.com>
2022-08-01 11:55:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -31,48 +31,49 @@ app.use(express.json())
 //middleware to read req.body.<params>
 //CREATE USER
 app.post("/createUser", async (req,res) => {
+    console.log(req.body)
 // const user = req.body.name;
 
-    var isProduction = false;
+//     var isProduction = false;
     
-    var strSimulator  = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
-    var strLive = "https://ipnpb.paypal.com/cgi-bin/webscr";
-    var paypalURL = strSimulator;
+//     var strSimulator  = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
+//     var strLive = "https://ipnpb.paypal.com/cgi-bin/webscr";
+//     var paypalURL = strSimulator;
     
-    if (isProduction)  paypalURL = strLive;
+//     if (isProduction)  paypalURL = strLive;
     
-    var payload = "cmd=_notify-validate&" + req.body;
+//     var payload = "cmd=_notify-validate&" + req.body;
   
-    var options =
-      {
-        "method" : "post",
-        "payload" : payload,
-      };
+//     var options =
+//       {
+//         "method" : "post",
+//         "payload" : payload,
+//       };
       
-    var resp = UrlFetchApp.fetch(paypalURL, options); //Handshake with PayPal - send acknowledgement and get VERIFIED or INVALID response
+//     var resp = UrlFetchApp.fetch(paypalURL, options); //Handshake with PayPal - send acknowledgement and get VERIFIED or INVALID response
     
-    if (resp == 'VERIFIED') {
-      if (e.parameter.payment_status  == 'Completed') {
-        if (e.parameter.receiver_email == 'receiver@email.com') {
-          //Implement paid amount validation. If accepting payments in multiple currencies, use e.parameter.exchange_rate to convert to reference currency (USD) if paid in any other currency
-          if (amountValid) {
+//     if (resp == 'VERIFIED') {
+//       if (e.parameter.payment_status  == 'Completed') {
+//         if (e.parameter.receiver_email == 'receiver@email.com') {
+//           //Implement paid amount validation. If accepting payments in multiple currencies, use e.parameter.exchange_rate to convert to reference currency (USD) if paid in any other currency
+//           if (amountValid) {
           
-            //All validated - can process the payment here
+//             //All validated - can process the payment here
   
-            if (!(processSuccess)) {
-              //Process of payment failed - raise notification to check it out
-            }
-          } else {
-            //Payment does not equal expected purchase value
-          }
-        } else {
-         //Request did not originate from my PayPal account 
-        }
-      } else {
-       //Payment status not Completed 
-      }
-    } else
-    {
-     //PayPal response INVALID 
-    }
+//             if (!(processSuccess)) {
+//               //Process of payment failed - raise notification to check it out
+//             }
+//           } else {
+//             //Payment does not equal expected purchase value
+//           }
+//         } else {
+//          //Request did not originate from my PayPal account 
+//         }
+//       } else {
+//        //Payment status not Completed 
+//       }
+//     } else
+//     {
+//      //PayPal response INVALID 
+//     }
 })
